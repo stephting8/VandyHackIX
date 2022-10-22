b8f21fe53717a706b90b31a3f9cc88cd2f59a886
lisaliuu <lisaliuu2016@gmail.com>
2022-10-22 00:09:50 -0500

    starting point

--- home.py
+++ home.py
@@ -1,5 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/list_rides')
+def rides():
+    return render_template('list_rides.html')
+
+@app.route('/create_rides')
+def createrides():
+     return render_template('create_rides.html')
+
+
+@app.route('/profile')
+def profile():
+     return render_template('profile.html')
+
+@app.route('/instructions')
+def instructions():
+     return render_template('instructions.html')
